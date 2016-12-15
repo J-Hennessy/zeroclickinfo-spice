43749aa98b7c4a43dc2fc09299d2690d5f2e626e
Zaahir Moolla <moollaza@duckduckgo.com>
2016-12-14 23:17:12 -0500

    Init REPL Spice

---
+++ lib/DDG/Spice/Repl.pm
@@ -0,0 +1,84 @@
+package DDG::Spice::Repl;
+
+use DDG::Spice;
+use strict;
+use warnings;
+
+spice is_cached => 1;
+spice proxy_cache_valid => '200 1d'; # defaults to this automatically
+
+spice wrap_jsonp_callback => 0;
+
+spice from => '(.*)/(.*)';
+spice to => 'http://eval.repl.it/eval';
+
+spice post_body => 'language=$1&code=$2';
+
+triggers end => 'repl';
+
+my @langs = qw(
+    apl
+    bloop
+    brainf
+    c
+    csharp
+    cpp
+    cpp11
+    coffeescript
+    emoticon
+    babel
+    fsharp
+    forth
+    go
+    java
+    javascript
+    jest
+    lolcode
+    lua
+    nodejs
+    php
+    python
+    python_turtle
+    python3
+    qbasic
+    roy
+    ruby
+    rust
+    scheme
+    swift
+    unlambda
+    web_project
+);
+
+my %aliases = (
+    'brain fuck' => 'brainf',
+    'brainfuck' => 'brainf',
+    'c#' => 'csharp',
+    'c++' => 'cpp',
+    'c++11' => 'cpp11',
+    'coffee script' => 'coffeescript',
+    'css' => 'web_project',
+    'es6' => 'babel',
+    'f#' => 'fsharp',
+    'html' => 'web_project',
+    'java script' => 'javascript',
+    'javascript es6' => 'babel',
+    'js es6' => 'babel',
+    'js' => 'javascript',
+    'node.js' => 'nodejs',
+    'node' => 'nodejs',
+    'python 2' => 'python',
+    'python 3' => 'python3',
+    'python turtle' => 'python_turtle',
+    'python with turtle' => 'python_turtle',
+    'python2' => 'python'
+);
+
+handle remainder => sub {
+
+    return unless $_;
+    return $_ if $_ ~~ @langs;
+    return $aliases{$_} if exists $aliases{$_};
+};
+
+1;
